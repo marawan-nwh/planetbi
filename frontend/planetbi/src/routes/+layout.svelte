24128e5566945d8073ab0218e9d7d938d04aa8c7
Marwan <marwan.nwh@gmail.com>
2024-09-10 23:48:47 +0300

    changes

--- frontend/planetbi/src/routes/+layout.svelte
+++ frontend/planetbi/src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <svelte:head>
-  <title>{_title === "" ? "PlanetBI" : _title + " - PlanetBI"}</title>
+  <title>{_title === "" ? "Plainbyte" : _title + " - Plainbyte"}</title>
 </svelte:head>
 
 <slot></slot>
