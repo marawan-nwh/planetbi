ec99ebcd768c1b6079ebfea61d32ea25c99b30e3
Marwan <marwan.nwh@gmail.com>
2024-09-17 21:43:58 +0300

    changes

--- frontend/planetbi/src/lib/components/header/component.svelte
+++ frontend/planetbi/src/lib/components/header/component.svelte
@@ -43,14 +43,14 @@
           </div>
         </div>
         <div class="separator"></div> -->
-    <a class="home item active" href="/home">
+    <!-- <a class="home item active" href="/home">
       <svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 24 24">
         <path
           d="M12 3s-6.186 5.34-9.643 8.232A1.041 1.041 0 0 0 2 12a1 1 0 0 0 1 1h2v7a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 .98.98 0 0 0-.383-.768C18.184 8.34 12 3 12 3z"
         ></path>
       </svg>
       Home
-    </a>
+    </a> -->
     <a class="store item" href="/store">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
         <path
